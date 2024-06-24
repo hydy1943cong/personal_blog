cf1031b37bd57975e1df90d44702355354c65091
Cong Wang <wangcong525@hotmail.com>
2024-06-23 21:56:04 -0400

    redirect page done

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,7 +12,17 @@ submit.addEventListener('click', function (event) {
         title: title.value,
         content: content.value.trim()
       };
+      if (username.value === '') {
+        alert("Username cannot be blank");
+      } else if (title.value === '') {
+        alert("Title cannot be blank");
+      } else if (content.value === '') {
+        alert('Content cannot be blank');
+      };
       localStorage.setItem('bloginfo', JSON.stringify(bloginfo));
+      location.replace("blog.html");
     }
 )
+
+
   
