ff1528b834653cd8236998b768abd31d98ca6806
Cong Wang <wangcong525@hotmail.com>
2024-06-23 23:51:10 -0400

    sucessfully display the message in the local storage

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,7 +1,24 @@
 const back=document.querySelector('#back');
+const author=document.querySelector('#username');
 
 back.addEventListener('click', function (event) {
     event.preventDefault();
       location.replace("index.html");
     }
 )
+
+
+function displayblogContent() {
+    const blogContent = JSON.parse(localStorage.getItem('bloginfo'));    
+    const blogContentElement = document.createElement('div'); 
+    const blogAuthorElement = document.createElement('p');
+    blogContentElement.innerHTML += `<h2>Title: ${blogContent.title}</h2>`;
+    blogContentElement.innerHTML += `<p>Content: ${blogContent.content}</p>`; 
+    blogContentElement.innerHTML += `<p>Uploaded by: ${blogContent.username}</p>`; 
+
+    // Choose where to display the content (replace with your desired location)
+    const mainElement = document.querySelector('main');
+    mainElement.appendChild(blogContentElement);
+  } 
+
+displayblogContent();
